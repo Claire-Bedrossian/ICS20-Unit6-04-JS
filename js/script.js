23f950c03b2f1bd3d5667ce333a34ab0a27044a1
Claire Bedrossian <113055648+Claire-Bedrossian@users.noreply.github.com>
2023-01-02 11:17:15 -0500

    added some function

--- js/script.js
+++ js/script.js
@@ -1,23 +1,33 @@
-// Copyright (c) 202X Claire Bedrossian All rights reserved
+// Copyright (c) 2023 Claire Bedrossian All rights reserved
 //
 // Created by: Claire Bedrossian
-// Created on: XXX 202X
+// Created on: Jan 2023
 // This file contains the JS functions for index.html
 
 "use strict"
 
 /**
- * Check servie worker.
+ * Check service worker.
  */
 if (navigator.serviceWorker) {
-  navigator.serviceWorker.register("/ICS2O-PWA-Test/sw.js", {
-    scope: "/ICS2O-PWA-Test/",
+  navigator.serviceWorker.register("/ICS20-Unit6-04-JS/sw.js", {
+    scope: "/ICS20-Unit6-04-JS/",
   })
 }
 
-/**
- * This function displays an alert.
- */
-function myButtonClicked() {
-  document.getElementById("hello-world").innerHTML = "<p>Hello, World!</p>"
+window.onload = function () {
+  // this calculates the volume of the sphere
+  const params = new URLSearchParams(document.location.search)
+
+  //input
+  const radius = parseFloat(params.get("r"))
+
+  //process
+  const volume = (4 / 3) * Math.PI * radius ** 3
+
+  //output
+  document.getElementById("radius").innerHTML =
+    "<h6>Radius = " + radius + "</h6)"
+  document.getElementById("volume").innerHTML =
+    "<h6>Volume is: " + volume.toFixed(2) + " cm³</h6>"
 }
